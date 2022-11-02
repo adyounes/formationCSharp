c334e10a1804caf5abbdb41228e9efceb0b0b213
Phonophage <hauzg.thib@gmail.com>
2022-11-02 10:34:20 +0100

    premier commit

---
+++ formationCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace formationCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
