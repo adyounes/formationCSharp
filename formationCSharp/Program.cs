83a6e81fdd297de43556e741980146d3e126b400
BusterLaw <zida932@hotmail.fr>
2022-11-02 11:15:39 +0100

    commit de reglage

--- formationCSharp/Program.cs
+++ formationCSharp/Program.cs
@@ -7,6 +7,7 @@ namespace formationCSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("test commit");
         }
     }
 }
