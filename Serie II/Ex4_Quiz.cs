4bee66fc357b1eae7273ca53c14c6c1d97cd59eb
buster <zida932@gmail.com>
2022-11-03 16:50:53 +0100

    modifs fin de journée 2

--- Serie II/Ex4_Quiz.cs
+++ Serie II/Ex4_Quiz.cs
@@ -8,7 +8,18 @@ namespace Serie_II
 {
     public struct Qcm
     {
-        //TODO
+        public string Question;
+        public string[] Reponses;
+        public int Solution;
+        public int Points;
+
+        public Qcm(string question, string[] reponses, int solution, int points)
+        {
+            Question = question;
+            Reponses = reponses;
+            Solution = solution;
+            Points = points;
+        }
     }
 
     public static class Quiz
@@ -16,18 +27,55 @@ namespace Serie_II
         public static void AskQuestions(Qcm[] qcms)
         {
             //TODO
+
         }
 
         public static int AskQuestion(Qcm qcm)
         {
+
             //TODO
+            if (QcmValidity(qcm))
+            {
+               
+                Console.WriteLine("Reponse invalide !");
+                Console.WriteLine("Entrez une nouvelle réponse !");
+                string input = Console.ReadLine();
+                // int answer;
+                // bool b = int.TryParse(input,out answer);
+                for (int i = 0; i < qcm.Solution; i++)
+                {
+
+                }
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine(qcm.Question);
+                for (int i = 0; i < qcm.Reponses.Length; i++)
+                {
+                    Console.Write(qcm.Reponses[i] + " ");
+                }
+
+                //qcm.Points += 1;
+            }
+
             return -1;
         }
 
         public static bool QcmValidity(Qcm qcm)
         {
-            //TODO
-            return false;
+
+            if (qcm.Solution <= 0 || qcm.Solution > qcm.Reponses.Length)
+            {
+                return false;
+            }
+
+            if (qcm.Points < 0)
+            {
+                return false;
+            }
+            return true;
+
         }
     }
 }
--- Serie II/Program.cs
+++ Serie II/Program.cs
@@ -16,22 +16,25 @@ namespace Serie_II
             Console.WriteLine("------------------------");
             int[] tableau = { 1, 2, 3, 4, 5, 6, 9, 12, 48, 654 };
             int[] tab2 = { };
-            Search.LinearSearch(tableau,654);
-            Search.LinearSearch(tableau ,22);
+            Search.LinearSearch(tableau, 654);
+            Search.LinearSearch(tableau, 22);
             Search.LinearSearch(tab2, 22);
             Console.WriteLine("------------------------");
             Console.WriteLine("Exo 1.2 : Recherche dichotomique");
             Console.WriteLine("------------------------");
-//            Search.BinarySearch(tableau, 6);
-//            Search.BinarySearch(tableau, 22);
+            //            Search.BinarySearch(tableau, 6);
+            //            Search.BinarySearch(tableau, 22);
             #endregion
 
             #region Exercice 3 - Crible d'Eratosthène
             Console.WriteLine("------------------------");
             Console.WriteLine("Exo 3 : Crible d'Eratosthène");
             Console.WriteLine("------------------------");
-            Eratosthene.EratosthenesSieve(100);
+            Eratosthene.EratosthenesSieve(1000);
             #endregion
+            Qcm qst = new Qcm("Quelle est l'année du sacre de Charlemagne ?", new string[4],2,0);
+            Quiz.AskQuestion(qst);
+
             // Keep the console window open
             Console.WriteLine("----------------------");
             Console.WriteLine("Press any key to exit.");
