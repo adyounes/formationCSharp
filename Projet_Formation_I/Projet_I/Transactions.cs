49fa5e814a57712578ca286091880a1447f3be7e
DVNO51 <mouillet.claude@outlook.com>
2022-11-10 11:54:31 +0100

    Passage .NETFramework v4.7.2

---
+++ Projet_Formation_I/Projet_I/Transactions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_I
+{
+    internal class Transactions
+    {
+        private readonly int _identifiant;
+        private double _montantTr;
+        private int _expediteur;
+        private int _destinataire;
+        private bool _status;
+        private bool _is_dupe;
+
+        public Transactions(int id, int expe, int desti, bool dupe, double tr = 0, double maxR = 1000)
+        {
+            _identifiant = id;
+            _montantTr = tr;
+            _expediteur = expe;
+            _destinataire = desti;
+            _is_dupe = dupe;
+        }
+
+        public int GetId()
+        {
+            return _identifiant;
+        }
+
+        public double GetMontantTr()
+        {
+            return _montantTr;
+        }
+
+        public int GetExpediteur()
+        {
+            return _expediteur;
+        }
+
+        public int GetDestinataire()
+        {
+            return _destinataire;
+        }
+
+        public bool GetStatus()
+        {
+            return _status;
+        }
+
+        public void SetStatus(bool newstatus)
+        {
+            _status = newstatus;
+        }
+
+        public bool IsDoublon()
+        {
+            return _is_dupe;
+        }
+
+    }
+}
